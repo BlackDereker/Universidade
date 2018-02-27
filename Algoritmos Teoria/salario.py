1e93b415e8df0a64dd4ccc84fbdf51f74cfbfb36
BlackDereker <blackdereker@gmail.com>
2018-02-27 08:44:54 -0300

    foo

---
+++ Algoritmos Teoria/salario.py
@@ -0,0 +1,6 @@
+salario = float(input("Digite o seu salario: "))
+percentual = float(input("Digite o percentual de aumento: "))
+
+aumento = salario * percentual / 100
+
+print("\nAumento: ", aumento)
