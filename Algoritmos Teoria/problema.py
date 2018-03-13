a871d37a7abda58762aec6a5905053d1b33032b7
BlackDereker <blackdereker@gmail.com>
2018-03-13 08:26:36 -0300

    foo

--- Algoritmos Teoria/problema.py
+++ Algoritmos Teoria/problema.py
@@ -1,22 +1,19 @@
 vel = float(input("Velocidade: "))
 
 limite = 80
-
 multa = 0
 
-if vel > limite:
-    if vel > limite * 1.35:
-        multa = 700
-    elif vel >= limite * 1.21:
-        multa = 580
-    elif vel >= limite * 1.11:
-        multa = 380
-    else:
-        multa = 180
-
-print("")
-
-if multa == 0:
+if vel <= 80:
     print("Nao foi multado")
+    exit()
+
+if vel > limite * 1.35:
+    multa = 700
+elif vel >= limite * 1.21:
+    multa = 580
+elif vel >= limite * 1.11:
+    multa = 380
 else:
-    print("Multa: R$ %.2f" % multa)
+    multa = 180
+
+print("\nMulta: R$ %.2f" % multa)
